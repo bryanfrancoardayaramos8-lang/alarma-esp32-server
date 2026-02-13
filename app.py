aaa89be9d0b1c9185eb69a60210354135d055f2b
bryanfrancoardayaramos8-lang <bryanfrancoardayaramos8@gmail.com>
2026-02-12 21:35:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, request
 import requests
+import os
 
 app = Flask(__name__)
 
-BOT_TOKEN = "8442245837:AAHP0MMdKOX5W5_Tywz2nqSu7fgnZ0z4wYA"
-CHAT_ID = "6011549713"
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+CHAT_ID = os.environ.get("CHAT_ID")
 
 @app.route("/")
 def home():
@@ -23,3 +24,7 @@ def alerta():
     r = requests.post(url, data=data)
 
     return r.text
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
